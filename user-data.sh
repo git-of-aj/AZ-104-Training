f48c5529ee1e1aedafce84445e18490aa71d0746
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-03-19 21:08:33 +0530

    Create user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Update package list and install Apache web server
+sudo apt update -y
+sudo apt install apache2 -y
+
+# Create directory for video page and index file
+sudo mkdir -p /var/www/html/video
+wget https://raw.githubusercontent.com/Ananyojha/AZ-104-Training/main/index.html
+sudo cp ./index.html /var/www/html/video/index.html
